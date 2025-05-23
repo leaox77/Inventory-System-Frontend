90146ab1c528af92d2d9356b68fe217db91f3060
leaox77 <leandroeguerdo@gmail.com>
2025-05-23 05:16:30 -0400

    ruta

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api/v1'
+const API_URL = import.meta.env.VITE_API_URL || 'inventory-system-backend-production-51be.up.railway.app/api/v1'
 
 const api = axios.create({
   baseURL: API_URL,
